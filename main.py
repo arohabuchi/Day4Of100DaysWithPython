e7fe8cb2a0006d912ba68240703be45362056ef2
arohabuchi <arohabuchi@gmail.com>
2023-09-04 23:23:13 -0700

    intro to datatypes in python

--- main.py
+++ main.py
@@ -2,17 +2,17 @@
 strv: str = "hello world"
 print(strv)
 
-print(strv[0])
-print(strv[0:5])
+print(strv[0]) #print the first character
+print(strv[0:5]) #print the first 5 character
 
 # intro to set: {}
 # set are unique collections of objects. there are to type
 
 # 1. set:mutable
 basket = {'banana', 'tomatoes', 'tomatoes', 'pear', 'mango'}
-print(basket)
-j = set('acacacerddderdsssw')
-print('j : ',j)
+print(basket) #print the whole set. duplicate are removed
+j = set('acacacerddderdsssw') #convert j to set
+print('j : ',j) #print the set j
 
 # 2. frozeset: immutable
 b = frozenset("abbcaeeces")
@@ -32,6 +32,22 @@ li=["hi", 48, 890,9.0,'man','obi','no','ye','winner','pear']
 l2=['hello','world']
 print(li)
 print(li[0:2])
-print(li*2)
-print(li + l2)
+print(li*2) #print li twice
+print(li + l2) #concatenate li and l2
 
+#dictionary {}
+#uses key value pair. can be asess using square bracket[]
+dic = {'gender':'male','age':'20'}
+print(dic)#print both the keys and values
+print(dic['gender']) #print the value of gender male
+print(dic.values())# print all the values
+print(dic.keys())# print all the keys
+
+
+# Turple()
+# they are immutable
+t1 =(1,2,3,34.90,'hi','hello')
+t2=('obi', 'importer','exporter')
+print(t1)
+print(t1[0]) #print first char in t1
+print(t1 + t2) #concatenate t1 and t2
